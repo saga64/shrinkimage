485703171991704b1f5b81fe93756b774ffc0dd2
Wei Zhang <sagamail@gmail.com>
2014-07-29 22:00:28 +0100

    git ignore file

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(0);
 if($_POST){
   $files = explode("\n", $_POST['files']);
   foreach($files as $file){
@@ -9,6 +9,7 @@ if($_POST){
     $filename = $pathinfo['basename'];
     if($pathinfo['extension']=='png'){
       $api_key = $_POST['api_key'];
+      $_SESSION['api_key'] = $api_key;
       $json = json_decode(exec("curl -i --user api:$api_key --data-binary @$filename https://api.tinypng.com/shrink"));
       file_put_contents($filename.'tmp', file_get_contents($json->output->url));
       if(filesize($filename.'tmp')>0){
@@ -32,7 +33,7 @@ if($_POST){
 <body>
 <form action="" method="post" >
 <label for="api_key">TinyPNG API Key</label><br/>
-<input type="text" name="api_key" value="" size="36"/><br/>
+<input type="text" name="api_key" value="<?=$_SESSION['api_key'];?>" size="36"/><br/>
 <label for="files">Image URL</label><br/>
 <textarea rows="10" name="files" cols="80">
 </textarea>
