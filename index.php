e9c6bcc18fb19ce7d5fd6b078779455f800190b4
Wei Zhang <sagamail@gmail.com>
2014-07-29 21:38:53 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+if($_POST){
+  $files = explode("\n", $_POST['files']);
+  foreach($files as $file){
+    $compo =  parse_url($file);
+    $pathinfo = pathinfo($compo['path']);
+    chdir($_SERVER['DOCUMENT_ROOT'].$pathinfo['dirname']);
+    $filename = $pathinfo['basename'];
+    if($pathinfo['extension']=='png'){
+      $api_key = $_POST['api_key'];
+      $json = json_decode(exec("curl -i --user api:$api_key --data-binary @$filename https://api.tinypng.com/shrink"));
+      file_put_contents($filename.'tmp', file_get_contents($json->output->url));
+      if(filesize($filename.'tmp')>0){
+          rename($filename, $filename.'_bak');//make backup file
+          rename($filename.'tmp', $filename);
+      }
+    }
+    elseif($pathinfo['extension']=='jpg'){
+      copy($filename, $filename.'_bak');
+      exec("convert -strip -quality 65% $filename $filename");
+    }
+    $message = "$filename has been resized to ".filesize($filename);
+  }
+}
+
+?>
+<html>
+<head>
+<title>Shrink PNG file size</title>
+</head>
+<body>
+<form action="" method="post" >
+<input type="text" name="api_key" value="" size="36"/>
+<textarea rows="10" name="files" cols="80">
+</textarea>
+<br/>
+<?php echo $message;?>
+<br/>
+<input type="submit" name="submit" value="Submit"/>
+</form>
+</body>
+</html>
